9490aa09dbe3cb5d9cdf2b368b4a766dfe292044
Manja McMills <manjamcmills@gmail.com>
2022-06-29 11:18:31 -0700

    3.6.5

--- datetime.py
+++
@@ -1,6 +0,0 @@
-#Import the datetime class from the datetime module.
-import datetime as dt
-#use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-#print the present time.
-print("The time right now is ", now)
